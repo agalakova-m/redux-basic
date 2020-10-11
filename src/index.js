b738d4d23500d93750d0e4f0565f45c81fade627
Marina Agalakova <agalakova.m.a@gmail.com>
2020-10-11 19:49:28 +0300

    doing redux things

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
-import { createStore } from './createStore';
+import { createStore } from 'redux';
 import { rootReducer } from './redux/rootReducer';
+import { increment, decrement } from './redux/actions';
 
 const counter = document.getElementById('counter');
 const addBtn = document.getElementById('add');
@@ -11,14 +12,18 @@ const themeBtn = document.getElementById('theme');
 const store = createStore(rootReducer, 0);
 
 addBtn.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' });
+  store.dispatch(increment());
 });
 
 subBtn.addEventListener('click', () => {
-  store.dispatch({ type: 'DECREMENT' });
+  store.dispatch(decrement());
 });
 
-asyncBtn.addEventListener('click', () => {});
+asyncBtn.addEventListener('click', () => {
+  setTimeout(() => {
+    store.dispatch(increment());
+  }, 2000);
+});
 
 themeBtn.addEventListener('click', () => {
   // document.body.classList.toggle('dark');
